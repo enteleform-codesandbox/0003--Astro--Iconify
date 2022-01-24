d90ef7fcd3f960f21bc0dd6d917b3fc0be50e052
Enteleform <enteleform@gmail.com>
2022-01-24 13:46:34 -0500

    init

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import Icons         from "unplugin-icons/vite"
+import AutoImport    from "unplugin-auto-import/vite"
+import IconsResolver from "unplugin-icons/resolver"
+
+
+const IconAliases = {
+	"awesome_":         "fa-solid",
+	"awesome-brands_":  "fa-brands",
+	"awesome-outline_": "fa-regular",
+}
+
+
+// @ts-check
+export default /** @type {import("astro").AstroUserConfig} */ ({
+	renderers: ["@astrojs/renderer-solid"],
+
+	vite: {
+		plugins: [
+
+			AutoImport({
+				resolvers: [
+					IconsResolver({prefix:"Icon_", extension:"tsx", alias:IconAliases}),
+					IconsResolver({prefix:"Icon_", extension:"md" , alias:IconAliases}),
+				],
+			}),
+
+			Icons({
+				compiler:     "solid",
+				defaultClass: "Icon",
+			}),
+
+		],
+	},
+});
---
+++ src/components/Icons.tsx
@@ -0,0 +1,12 @@
+import "./Icons.css"
+
+
+export default function Icons(){
+	return (
+		<>
+			<Icon_Awesome_Star       />
+			<Icon_AwesomeBrands_Html5/>
+			<Icon_AwesomeOutline_Star/>
+		</>
+	)
+}
